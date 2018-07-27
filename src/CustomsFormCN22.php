36dbe3ae64ab35468cf67ad5873005641c6fee73
Paul Hinds <greasypablo@users.noreply.github.com>
2018-07-27 14:56:14 +0100

    Made cn22 slightly bigger

--- src/CustomsFormCN22.php
+++ src/CustomsFormCN22.php
@@ -1,14 +1,14 @@
 <?php namespace fhindsthejewellers\GBCustomsForm;
 
 class CustomsFormCN22 extends CustomsForm {
-    private $formPosition = array (41,47,53,59);
+    private $formPosition = array (42,48,54,60);
     
-    const DESCWIDTH = 57;
+    const DESCWIDTH = 59;
     
     public function __construct(array $nonProductDetails) {
         $this->pdf = new \FPDF('P','mm',array(100,105));
         $this->pdf->AddPage();
-        $this->pdf->image(__DIR__."/cn22.png",2,5,96);
+        $this->pdf->image(__DIR__."/cn22.png",0,5,100);
         $this->pdf->SetFont('Courier','',10);
         $this->pdf->SetMargins(1,0);
         $this->pdf->SetAutoPageBreak(false,0);
@@ -18,16 +18,16 @@ class CustomsFormCN22 extends CustomsForm {
     protected function TickGoodsBox() {
         switch ($this->goodsType) {
             case self::GIFT:
-                $this->pdf->SetXY(7,26);
+                $this->pdf->SetXY(5,27);
                 break;
             case self::DOCUMENTS:
-                $this->pdf->SetXY(7,30);
+                $this->pdf->SetXY(5,31);
                 break;
             case self::COMMERCIALSAMPLE:
-                $this->pdf->SetXY(38,26);
+                $this->pdf->SetXY(37,26);
                 break;
             default:
-                $this->pdf->SetXY(38,30);
+                $this->pdf->SetXY(37,31);
                 break;
         }
         $this->pdf->SetFont('Courier','',10);
@@ -36,8 +36,8 @@ class CustomsFormCN22 extends CustomsForm {
     
     protected function SignAndDate() {
         $this->pdf->SetFont('Courier','',10);
-        $this->pdf->SetXY(35,94);
-        $this->pdf->Cell(57,0,$this->date.' '.$this->name,0,0);
+        $this->pdf->SetXY(35,97);
+        $this->pdf->Cell(60,0,$this->date.' '.$this->name,0,0);
     }
     
     protected function WriteTotalWeight() {
@@ -46,13 +46,13 @@ class CustomsFormCN22 extends CustomsForm {
         } else {
             $mass=$this->totalProductMass;
         }
-        $this->pdf->SetXY(61,77);
+        $this->pdf->SetXY(62,78);
         $this->pdf->SetFont('Courier','',10);
         $this->pdf->Cell(19,0,$mass,0,0);
     }
     
     protected function WriteTotalValue() {
-        $this->pdf->SetXY(80,72);
+        $this->pdf->SetXY(82,74);
         $this->pdf->SetFont('Courier','',8);
         $this->pdf->Cell(15,5,$this->totalValue,0,2);
         $this->pdf->Cell(15,0,$this->totalValueCurrency,0,0);
@@ -66,7 +66,7 @@ class CustomsFormCN22 extends CustomsForm {
         $currency = "GBP";
         foreach ($products as $key=>$product) {
             $descfont = 8;
-            $this->pdf->SetXY(4,$this->formPosition[$key]);
+            $this->pdf->SetXY(2.3,$this->formPosition[$key]);
             $this->pdf->SetFont('Courier','',$descfont);
             $index = $key + 1;
             $description = "$index: {$product["name"]} x{$product["salesQty"]}";
@@ -96,17 +96,17 @@ class CustomsFormCN22 extends CustomsForm {
             } else {
                 $originHs2 = $originHs2 . "$index:({$hsCode};{$product["origin_country"]}) ";
             }
-            $this->pdf->Cell(57,0,$description,0,0);
+            $this->pdf->Cell(59,1,$description,0,0);
             $this->pdf->SetFont('Courier','',8);
-            $this->pdf->Cell(19,0,$mass,0,0);
+            $this->pdf->Cell(20,0,$mass,0,0);
             $this->pdf->SetFont('Courier','',8);
-            $this->pdf->Cell(15,0,$value.$product["value"]["currency"],0,0);
+            $this->pdf->Cell(16,0,$value.$product["value"]["currency"],1,0);
             
         }
-        $this->pdf->SetXY(4,74);
+        $this->pdf->SetXY(3,76.7);
         $this->pdf->SetFont('Courier','',9);
         $this->pdf->Cell(57,0,$originHs1,0,0);
-        $this->pdf->SetXY(4,78);
+        $this->pdf->SetXY(3,80.7);
         $this->pdf->Cell(57,0,$originHs2,0,0);
 
     }
